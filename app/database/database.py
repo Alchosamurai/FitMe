5e8d8822a7897848929c318ebd16022239463985
Alchosamurai <alchosan.py@gmail.com>
2025-08-22 13:48:31 +0600

    feat(database): добавлен класс для работы с базой данных с использованием SQLAlchemy

---
+++ app/database/database.py
@@ -0,0 +1,14 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine("sqlite:///database.db")
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = Session()
+    try:
+        yield db
+    finally:
+        db.close()
