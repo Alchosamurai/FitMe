5389a4d775812e63aeccce87ca898365396b61be
Alchosamurai <alchosan.py@gmail.com>
2025-08-30 05:01:01 +0600

    Доработана структура БД

--- app/models/user.py
+++ app/models/user.py
@@ -1,16 +1,21 @@
-from sqlalchemy import Column, Integer, String, Float
+from enum import Enum
+from sqlalchemy import Column, Integer, String, Float, Boolean, Enum as SQLEnum
 from app.database.database import Base
 
+class Activity(Enum):
+    LOW = 0
+    MEDIUM = 1
+    HIGHT = 2
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
     name = Column(String)
     age = Column(Integer)
-    gender = Column(String)
+    gender = Column(Boolean)
     weight = Column(Float)
     height = Column(Float)
-    activity = Column(String)
+    activity = Column(SQLEnum(Activity), default=Activity.MEDIUM)
     goal = Column(String)
     calories = Column(Float)
     fat = Column(Float)
--- app/telebot/shemas/user_states.py
+++ app/telebot/shemas/user_states.py
@@ -6,3 +6,8 @@ class UserState(Enum):
     ADD_PRODUCT = "add_product"
     ADD_PRODUCT_AMOUNT = "add_product_amount"
     ADD_PRODUCT_DATE = "add_product_date"
+    ADD_SELF_AGE = "add_self_age"
+    ADD_SELF_WEIGHT = "add_self_weight"
+    ADD_SELF_ACTIVITY = "add_self_activity"
+    ADD_CUSTOM_PRODUCT_NAME = "add_custom_product_name"
+    ADD_CUSTOM_PRODUCT_CAL = "add_custom_product_cal"
