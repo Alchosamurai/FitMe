a199687fad9bde259dabd3fdbe6e39bdf0274af7
Alchosamurai <alchosan.py@gmail.com>
2025-09-10 12:32:11 +0600

    feat(database): добавлена асинхронная поддержка работы с базой данных и декоратор для управления транзакциями

--- app/database/database.py
+++ app/database/database.py
@@ -1,4 +1,6 @@
-from sqlalchemy import create_engine
+from typing import Callable, Optional
+from sqlalchemy import create_engine, text
+from sqlalchemy.ext.asyncio import async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -13,3 +15,49 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+a_engine = create_async_engine("sqlite+aiosqlite:///database.db")
+a_sessionmaker = async_sessionmaker(a_engine, expire_on_commit=False)
+
+
+async def a_get_db():
+    async with a_sessionmaker() as db:
+        yield db
+
+
+def a_connection(isolation_level: Optional[str] = None):
+    """
+    Декоратор для управления асинхронными транзакциями с настраиваемым уровнем изоляции.
+
+    Args:
+        isolation_level: Уровень изоляции транзакции:\n
+        "READ COMMITTED"\n
+        "REPEATABLE READ"\n
+        "SERIALIZABLE"\n
+        Если None, используется
+        уровень по умолчанию базы данных.
+    """
+
+    def decorator(method: Callable):
+        async def wrapper(*args, **kwargs):
+            async with a_sessionmaker() as session:
+                try:
+                    if isolation_level:
+                        await session.execute(
+                            text(f"BEGIN TRANSACTION ISOLATION LEVEL {isolation_level}")
+                        )
+
+                    result = await method(*args, **kwargs, session=session)
+                    await session.commit()
+                    return result
+
+                except Exception as e:
+                    await session.rollback()
+                    raise e
+                finally:
+                    await session.close()
+
+        return wrapper
+
+    return decorator
